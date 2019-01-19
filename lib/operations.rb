cffd170b2272b2e19be2fd5eba42d400020b4c93
Ming Duan <11203duan@gmail.com>
2019-01-19 20:16:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+return "true" if speed > 60
 end
 
+def unsafe(speed)
+	return "true" if speed < 40
+end
+
+def unsafe?(speed)
+	return "false" if speed < 60 && speed > 40
+end
+
+def not_safe?(speed)
+	return "true" if speed > 60 || speed < 40
+end
+
+def no
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
